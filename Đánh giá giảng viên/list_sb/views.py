f161857e0ab7b2b032c232342040000487d230e9
lac <hgiang16260@gmail.com>
2026-03-17 00:15:23 +0700

    h

--- Đánh giá giảng viên/list_sb/views.py
+++ Đánh giá giảng viên/list_sb/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, session
 import sqlite3
 
 from . import list_sb_bp
@@ -8,8 +8,10 @@ def danh_sach():
     # khai báo con trỏ trỏ tới sinhvien.db
     conn = sqlite3.connect(studentdb)
     cursor = conn.cursor()
+    # Lấy mã sinh viên từ user
+    ma_sv = session.get('ma_sinh_vien')
     # Truy vấn câu lệnh SQL
-    cursor.execute("SELECT * FROM HocPhan WHERE ma_hoc_phan IN (SELECT ma_hoc_phan FROM DangKyHP WHERE ma_sinh_vien = 'SV001');")
+    cursor.execute("""SELECT * FROM HocPhan WHERE ma_hoc_phan IN (SELECT ma_hoc_phan FROM DangKyHP WHERE ma_sinh_vien = ?)""", (ma_sv,))
     data = cursor.fetchall()
     # Đóng kết nối db
     conn.close()
